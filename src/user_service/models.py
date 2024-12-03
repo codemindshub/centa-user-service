9017209dc6331f945300ac0544f36be25d17a28c
thenoblet <appiahpatricknoblet@gmail.com>
2024-12-03 20:11:24 +0000

    CHORE(models): add custom user, organisation, roles, and permissions models
    This commit introduces a comprehensive set of models to establish the core data structure for the application. These models include:
    
    - **User Model**: A custom user model (`User`) inheriting from `AbstractBaseUser`, with fields like `username`, `email`, `password`, `role`, and other attributes. This model includes an associated manager (`CustomUserManager`) to handle user creation, activation, and updates.
    - **Organisation Model**: Represents organisations, including fields for the name, logo, address, contact information, and email.
    - **UserProfile Model**: A one-to-one relationship with the `User` model to store user-specific profile data like avatars.
    - **Role Model**: Defines roles that users can have, with a `name` and `description`.
    - **Permission Model**: Defines permissions that can be assigned to roles, with a `name` and `description`.
    - **RolePermission Model**: A many-to-many relationship between roles and permissions, with constraints to prevent duplicate entries.
    - Custom database table names are provided for clarity.
    - Unique constraints and validators are applied to ensure data integrity.
    
    These additions lay the foundation for user authentication, role-based access control, and organisational management within the application.
    
    **Error Handling Improvements:**
    - Exception handling in manager methods like `update_user` has been improved for better alignment with Django's conventions by using `self.model.DoesNotExist` instead of generic `ValueError`.
    
    This commit represents the backbone of the application's data architecture, providing a flexible and scalable structure for future development.

--- src/user_service/models.py
+++ src/user_service/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import (
     BaseUserManager
 )
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import EmailValidator
+from django.core.exceptions import ValidationError
+from uuid import uuid4
 
 
 class CustomUserManager(BaseUserManager):
@@ -12,7 +16,8 @@ class CustomUserManager(BaseUserManager):
     Methods:
         - create_user: Creates a new regular user.
         - create_superuser: Creates a new superuser with elevated permissions.
-        - remove_user: Deactivates a user based on their email.
+        - deactivate_user: Deactivates a user based on their email.
+        - activate_user: Activates a user based on their email
         - update_user: Updates user information by email.
     """
 
@@ -27,12 +32,27 @@ class CustomUserManager(BaseUserManager):
         if password is None:
             raise ValueError("The password field is required")
 
-
         email = self.normalize_email(email)
         user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        """
+        Create and save a superuser with the given email, username,
+        and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(email, username, password, **extra_fields)
 
     def deactivate_user(self, email):
         """
@@ -72,8 +92,108 @@ class CustomUserManager(BaseUserManager):
                     setattr(user, field, value)
             user.save(using=self._db)
         except self.model.DoesNotExist:
-            raise ValueError(f"User with email: {email} does not exist")
+            raise self.model.DoesNotExist(f"User with email: {email} does not exist")
 
 
+class User(AbstractBaseUser):
+    class UserStation(models.TextChoices):
+        WAREHOUSE = "WH", _("warehouse")
+        SHOP = "SP", _("shop")
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    username = models.CharField(max_length=255, unique=True, null=False, blank=False)
+    email = models.CharField(max_length=255, unique=True, blank=False, null=False, validators=[EmailValidator()])
+    password = models.CharField(max_length=255, blank=False, null=False)
+    firstname = models.CharField(max_length=255)
+    middlename = models.CharField(max_length=255, blank=True)
+    lastname = models.CharField(max_length=255, blank=False, null=False)
+    role = models.CharField(max_length=255, blank=False, null=False)
+    organisation_id = models.OneToOneField(Organisation, on_delete=models.CASCADE, null=False, blank=False)
+    user_station = models.CharField(max_length=2, choices=UserStation, blank=False, null=False)
+    station_id = models.UUIDField(blank=False, null=True)
+    is_active = models.BooleanField(default=True, blank=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "firstname", "lastname", "role", "user_station"]
+
+    class Meta:
+        db_table = "user"
+        indexes = [models.Index(fields=["id"]),]
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return f"{self.username} with email: {self.email}"
 
 
+class UserProfile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, null=False, blank=False)
+    avatar = models.ImageField(upload_to="")
+
+    class Meta:
+        db_table = "profiles"
+
+
+class Organisation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    logo = models.ImageField(upload_to="")
+    address = models.CharField(max_length=255, blank=True)
+    contact = models.CharField(max_length=255, blank=True)
+    email = models.CharField(max_length=255, blank=True, validators=[EmailValidator()])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "organisation"
+
+    def __str__(self):
+        return self.name
+
+
+class Role(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "role"
+
+    def __str__(self):
+        return self.name
+
+
+class Permission(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "permission"
+
+    def __str__(self):
+        return self.name
+
+
+class RolePermission(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, unique=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    permission = models.ForeignKey(Permission, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "role_permission"
+        unique_together = ("role", "permission")
+
+    def __str__(self):
+i        return f"{self.role.name} - {self.permission.name}"
